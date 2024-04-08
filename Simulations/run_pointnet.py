a75d410262627611ad21be89465fc9f3b3a68126
MargauxCalice <margaux-calice@orange.fr>
2024-04-08 16:12:12 +0200

    run pointnet network according to config file

---
+++ Simulations/run_pointnet.py
@@ -0,0 +1,19 @@
+from bmtk.simulator import pointnet
+import nest
+from bmtk.analyzer.spike_trains import plot_raster
+from bmtk.analyzer.compartment import plot_traces
+
+def main(config_file):
+    configure = pointnet.Config.from_json(config_file)
+    configure.build_env()
+
+    network = pointnet.PointNetwork.from_config(configure)
+    sim = pointnet.PointSimulator.from_config(configure, network)
+    sim.run()
+    #pointnet.nrn.quit_execution()
+    _ = plot_raster(config_file=config_file, group_by='pop_name', show=True)
+    _ = plot_traces(config_file=config_file,report_name='V_m_report')
+
+
+if __name__ == '__main__':
+    main('../Config_files/config.l4_10percent.json')
