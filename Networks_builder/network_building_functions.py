7b994fedbd0221b67de6e97489056b6e227a63a2
MargauxCalice <margaux-calice@orange.fr>
2024-04-18 21:44:39 +0200

    updated arguments

--- Networks_builder/network_building_functions.py
+++ Networks_builder/network_building_functions.py
@@ -8,9 +8,9 @@ import json
 from position_building_V1 import get_population_loc_df,create_layer_population_loc,add_nodes_V1_in_nrrd
 from edges_V1_functions import distance,distance_connection,distance_edges_within
 
-def build_network (dict_node_path,factor,df_connection_info_path,dict_types,n_synapses,output_dir) : 
+def build_network (dict_node_path,factor,df_connection_info_path,dict_types,n_synapses,output_dir,layer_id) : 
     net_layers,dataframes= add_nodes_V1_in_nrrd(dict_node_path,factor)
-    v1=distance_edges_within(net_layers[2],df_connection_info_path,dict_types,n_synapses)
+    v1=distance_edges_within(net_layers[layer_id],df_connection_info_path,dict_types,n_synapses)
     v1.build()
     v1.save(output_dir)
 
